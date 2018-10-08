cb31513a3d048d01e7f2952c4bfa4d6e3972b7dc
MichaelTheTall <michaeljhnewman@gmail.com>
2018-10-08 15:55:03 +0100

    init commit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,29 @@
+public class Calculator {
+//    private int number1;
+//    private int number2;
+//    private int result;
+
+    public Calculator(){
+
+    }
+
+    public int add(int number1, int number2) {
+        int result = number1 + number2;
+        return result;
+    }
+
+    public int subtract(int number1, int number2) {
+        int result = number1 - number2;
+        return result;
+    }
+
+    public int multiply(int number1, int number2) {
+        int result = number1 * number2;
+        return result;
+    }
+
+    public int divide(int number1, int number2) {
+        int result = number1 / number2;
+        return result;
+    }
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,33 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    Calculator calc;
+
+    @Before
+    public void before() { calc = new Calculator(); }
+
+    @Test
+    public void hasAdd() {
+        assertEquals(3, calc.add(1,2));
+    }
+
+    @Test
+    public void hasSubtract() {
+        assertEquals(1, calc.subtract(3,2));
+    }
+
+    @Test
+    public void hasMultiply() {
+        assertEquals(10, calc.multiply(2,5));
+    }
+
+    @Test
+    public void hasDivide() {
+        assertEquals(4, calc.divide(16,4));
+    }
+
+}

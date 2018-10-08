8abedc94259a866f0a07e2b7d0c1831d2e933ca4
MichaelTheTall <michaeljhnewman@gmail.com>
2018-10-08 16:24:37 +0100

    printer complete

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,2 +1,45 @@
 public class Printer {
+    private int toner;
+    private int paper;
+
+    public Printer() {
+        this.toner = 1000;
+        this.paper = 500;
+    }
+    public int getPaper() {
+        return paper;
+    }
+
+    public void setPaper(int paper) {
+        this.paper = paper;
+    }
+    public int getToner() {
+        return toner;
+    }
+
+    public void setToner(int toner) {
+        this.toner = toner;
+    }
+
+
+    public String print(int pages, int copies) {
+        int total = pages * copies;
+        if (total > this.paper) {
+            return "The printer does not have enough paper!";
+        } else if (total > this.toner) {
+            return "The printer does not have enough ink!";
+        } else {
+            this.paper -= total;
+            this.toner -= total;
+            return "Print Sucessful!";
+        }
+    }
+
+    public void refillPaper() {
+        this.paper = 500;
+    }
+
+    public void refillToner() {
+        this.toner = 1000;
+    }
 }
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -1,2 +1,60 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PrinterTest {
+
+    Printer printer;
+
+    @Before
+    public void before() {
+        printer = new Printer();
+    }
+
+    @Test
+    public void hasToner() {
+        assertEquals(1000, printer.getToner());
+    }
+
+    @Test
+    public void hasPaper() {
+        assertEquals(500, printer.getPaper());
+    }
+
+    @Test
+    public void canPrint() {
+        assertEquals("Print Sucessful!", printer.print(2,1));
+    }
+
+    @Test
+    public void printUsesTonerAndPaper() {
+        printer.print(5, 10);
+        assertEquals(450, printer.getPaper());
+        assertEquals(950, printer.getToner());
+    }
+
+    @Test
+    public void cantPrintNoPaper() {
+        assertEquals("The printer does not have enough paper!", printer.print(100,6));
+    }
+
+    @Test
+    public void cantPrintNoInk() {
+        printer.setToner(100);
+        assertEquals("The printer does not have enough ink!", printer.print(100, 2));
+    }
+
+    @Test
+    public void canRefillPaper() {
+        printer.refillPaper();
+        assertEquals(500, printer.getPaper());
+    }
+
+    @Test
+    public void canRefillToner() {
+        printer.refillToner();
+        assertEquals(1000, printer.getToner());
+    }
 }
+
